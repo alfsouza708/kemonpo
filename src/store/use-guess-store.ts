b1eb00bfee3176a8ab7dca3cd4fcf12692ba01e3
Lucas Souza <alfsouza708@gmail.com>
2024-10-30 16:51:35 -0300

    feat: add persistent store

--- src/store/use-guess-store.ts
+++ src/store/use-guess-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 import { Pokemon } from "@/lib/types";
 import { pokemonList } from "@/lib/data";
@@ -10,30 +11,34 @@ type State = {
   history: Pokemon[];
 };
 
-type Action = {
-  updateChosen: (chosen: State["chosen"]) => void;
-  updateHistory: (history: State["history"]) => void;
-  updateAvailable: (available: State["available"]) => void;
-  newGame: () => void;
-};
-
 const initialState = {
   chosen: getRandomPokemon(pokemonList),
   available: pokemonList,
   history: [],
 };
 
-export const useGuessStore = create<State & Action>((set) => ({
-  chosen: initialState.chosen,
-  available: initialState.available,
-  history: initialState.history,
-  updateChosen: (chosen) => set(() => ({ chosen: chosen })),
-  updateHistory: (history) => set(() => ({ history: history })),
-  updateAvailable: (available) => set(() => ({ available: available })),
-  newGame: () =>
-    set(() => ({
-      chosen: getRandomPokemon(pokemonList),
-      available: pokemonList,
-      history: [],
-    })),
-}));
+export const useGuessStore = create(
+  persist(
+    (set) => ({
+      chosen: initialState.chosen,
+      available: initialState.available,
+      history: initialState.history,
+      updateChosen: (chosen: State["chosen"]) =>
+        set(() => ({ chosen: chosen })),
+      updateHistory: (history: State["history"]) =>
+        set(() => ({ history: history })),
+      updateAvailable: (available: State["available"]) =>
+        set(() => ({ available: available })),
+      newGame: () =>
+        set(() => ({
+          chosen: getRandomPokemon(pokemonList),
+          available: pokemonList,
+          history: [],
+        })),
+    }),
+    {
+      name: "pokemon-guess",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
